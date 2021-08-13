70799796c64cabb694108802d8a905b3d3bf2cdd
gonzaloarayav <gonzaloarayav@hotmail.com>
2021-08-13 00:51:15 -0400

    deploy bsale heroku

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
     host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user: 'bsale_test',
     password: 'bsale_test',
-    database: 'bsale_test'
+    database: 'bsale_test',
 })
 
 mysqlConnection.connect(function(err){
