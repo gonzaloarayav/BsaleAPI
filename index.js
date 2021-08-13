a93d8391cdf7f8029b484c9148bac7692e2aebe5
gonzaloarayav <gonzaloarayav@hotmail.com>
2021-08-13 00:30:35 -0400

    deploy bsale heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 });
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 // Middlewares
 app.use(express.json());
